8d1e3d331556c40d096de11c0d097bb7d9d40a74
Anders Abel <anders@abel.nu>
2014-05-16 10:21:45 +0200

    Added comments clarifying instance creation.

--- DummyOwinAuth/DummyAuthenticationMiddleware.cs
+++ DummyOwinAuth/DummyAuthenticationMiddleware.cs
@@ -9,14 +9,14 @@ using System.Threading.Tasks;
 
 namespace DummyOwinAuth
 {
+    // One instance is created when the application starts.
     class DummyAuthenticationMiddleware : AuthenticationMiddleware<DummyAuthenticationOptions>
     {
         public DummyAuthenticationMiddleware(OwinMiddleware next, IAppBuilder app, DummyAuthenticationOptions options)
             : base(next, options)
-        {
+        { }
 
-        }
-
+        // Called for each request, to create a handler for each request.
         protected override AuthenticationHandler<DummyAuthenticationOptions> CreateHandler()
         {
             return new DummyAuthenticationHandler();
